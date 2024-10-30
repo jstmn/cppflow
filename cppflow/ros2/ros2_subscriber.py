2aa50e646aed8fa36316b4dc6ee2e25360316fd0
Jeremy Morgan <jmorgan@theaiinstitute.com>
2024-10-30 14:07:39 -0400

    removed matplotlib dep, comes from jrl now. updates to ros2_subscriber.py

--- cppflow/ros2/ros2_subscriber.py
+++ cppflow/ros2/ros2_subscriber.py
@@ -3,16 +3,36 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String  # Adjust to the message type you want to listen to
 from cppflow.plan import Plan
-from cppflow_msgs.msg import CppFlowPlanningQuery
+from cppflow_msgs.srv import CppFlowQuery
+
+
+
+# string base_frame
+# string end_effector_frame
+# CppFlowProblem[] problems
+# ---
+# trajectory_msgs/JointTrajectory trajectories
+# bool[] success
+# string[] errors
 
 
 class SubscriberNode(Node):
     def __init__(self):
-        super().__init__('listener_node')  # Node name
-        self.subscription = self.create_subscription(String, '/cppflow_planning_query', self.listener_callback, 10)
+        super().__init__('cppflow_query_server')
+        self.srv = self.create_service(CppFlowQuery, '/cppflow_planning_query', self.query_callback)
+        self.get_logger().info("CppFlowQuery service server started...")
+    
+    def query_callback(self, request, response):
+        # Log the incoming request data
+        self.get_logger().info(f"Received request: base_frame={request.base_frame}, "
+                               f"end_effector_frame={request.end_effector_frame}, "
+                               f"number of problems={len(request.problems)}")
 
-    def listener_callback(self, msg):
-        self.get_logger().info(f"Received message: '{msg.data}'")
+        # TODO: Run CppFlow and convert the result to a JointTrajectory
+        response.trajectories = []
+        response.success = [False] * len(request.problems)
+        response.errors = ['unimplemented'] * len(request.problems)
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
