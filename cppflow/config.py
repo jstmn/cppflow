f987fafb7e9de1ac0dbcefc6711955eeb0fa8661
Jeremy Morgan <jmorgan@theaiinstitute.com>
2024-11-12 13:43:27 -0500

    added hydra

--- cppflow/config.py
+++ cppflow/config.py
@@ -12,14 +12,6 @@ torch.set_default_device(DEVICE)
 VERBOSITY = 2
 
 # Success criteria
-SUCCESS_THRESHOLD_translation_ERR_MAX_CM = 0.01  # 0.1mm
-SUCCESS_THRESHOLD_translation_ERR_MAX_MM = SUCCESS_THRESHOLD_translation_ERR_MAX_CM * 10
-SUCCESS_THRESHOLD_rotation_ERR_MAX_DEG = 0.1  #
-
-SUCCESS_THRESHOLD_mjac_CM = 2  # for prismatic joints
-# SUCCESS_THRESHOLD_mjac_CM = 1.5
-# SUCCESS_THRESHOLD_mjac_DEG = 7
-SUCCESS_THRESHOLD_mjac_DEG = 3  # for revolute joints
 
 # LM optimization
 SELF_COLLISIONS_IGNORED = False
